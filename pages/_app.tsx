8d4a7c71082e9bcb2eab6ddcfd80e2d223e76cc8
mrhoodz <hoodzt7@gmail.com>
2022-12-28 13:53:32 +0200

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Layout>
         <Component {...pageProps} />
-        <Analytics/>
       </Layout>
+      <Analytics/>
     </>
   );
 }
