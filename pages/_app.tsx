31bf2a5140ac5673e298862d6baa0deb03485ed1
mrhoodz <hoodzt7@gmail.com>
2022-12-28 13:50:28 +0200

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import "../styles/globals.css";
 import "../styles/global.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics mode={"production"} />
+      </Layout>
+    </>
   );
 }
