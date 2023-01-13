bab641291d0764ccc245770281cbed4361f8232e
mrhoodz <hoodzt7@gmail.com>
2023-01-13 15:06:09 +0200

    13/01/2022 06:43

--- components/layout/line.tsx
+++ components/layout/line.tsx
@@ -3,14 +3,14 @@ import style from "./line.module.scss";
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
 export default function line() {
-  const { scrollYProgress } = useScroll();
+  // const { scrollYProgress } = useScroll();
 
   return (
     <>
-      {/* <div className={style.line}>line</div> */}
-      <motion.div style={{ scaleX: scrollYProgress }} className={style.line}>
+      <div className={style.line}>line</div>
+      {/* <motion.div style={{ scaleX: scrollYProgress }} className={style.line}>
         line
-      </motion.div>
+      </motion.div> */}
     </>
   );
 }
